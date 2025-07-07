b30282e8a4a83c78bb2fedf1480e45f54c64d06e
BRYAMMMMM <bperaltan@est.ups.edu.ec>
2025-07-07 18:50:00 -0500

    Creacion del diseno del login

--- front-end/stocky-ia-front/src/app/pages/login/login.ts
+++ front-end/stocky-ia-front/src/app/pages/login/login.ts
@@ -1,11 +1,16 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  standalone: true,
   templateUrl: './login.html',
-  styleUrl: './login.scss'
+  styleUrl: './login.scss',
 })
 export class Login {
+  constructor(private router: Router) {}
 
+  goToHome() {
+    this.router.navigate(['/home']);
+  }
 }
