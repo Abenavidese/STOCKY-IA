222cf381b1a71f255a6ee63f183f21c769eda24d
HenryGranda <huambi123@gmail.com>
2025-07-07 23:50:44 -0500

    feat(core): configuración base de Celery y Redis

---
+++ back-model/core/celery_config.py
@@ -0,0 +1,11 @@
+from celery import Celery
+
+celery_app = Celery(
+    "tasks",
+    broker="redis://localhost:6379/0",
+    backend="redis://localhost:6379/0"
+)
+
+celery_app.conf.task_routes = {
+    "services.tasks.train_model": {"queue": "training-queue"}
+}
